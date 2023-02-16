2695352470f37454cb357970f13ba8d29b68c49d
yostintejaby <yostintejaxun20@.com>
2023-02-16 17:26:43 -0600

    project update

--- src/components/AppCards.jsx
+++ src/components/AppCards.jsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
 import Cards from "./Cards";
+import Load from './Load'
 import "../css/appCards.css";
 
 function AppCards() {
   const [usuarios, setUsuarios] = useState([]);
+  const [load, setLoad] = useState(false);
 
   useEffect(() => {
     getUsuarios();
@@ -13,15 +15,20 @@ function AppCards() {
     const request = await fetch("https://reqres.in/api/users?delay=1");
     const response = await request.json();
     setUsuarios(response.data);
+    setLoad(true);
   };
 
-  return (
-    <div className="container">
-      {usuarios.map((usuarios) => (
-        <Cards key={usuarios.id} usuarios={usuarios} />
-      ))}
-    </div>
-  );
+  const Card = () => {
+    return (
+      <div className="container">
+        {usuarios.map((usuarios) => (
+          <Cards key={usuarios.id} usuarios={usuarios} />
+        ))}
+      </div>
+    );
+  };
+
+  return <>{load ? <Card/> : <Load/>}</>
 }
 
 export default AppCards;
---
+++ src/components/Load.jsx
@@ -0,0 +1,5 @@
+import '../css/load.css'
+
+export default function Load() {
+  return <span class="loader"></span>;
+}
